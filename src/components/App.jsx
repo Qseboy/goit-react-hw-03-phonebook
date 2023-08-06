9ec61ba19efc1c5b71359333660d4bbc5ca03ef8
Nikolay Shein <qseboy1998@gmail.com>
2023-08-07 00:38:00 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,26 @@ import { nanoid } from 'nanoid';
 const { Component } = require('react');
 
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
 
 export class App extends Component {
   state = { ...INITIAL_STATE };
+
+  componentDidMount() {
+    const storageContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (storageContacts) {
+      this.setState({ contacts: storageContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   // update contacts
   updateState = (name, number) => {
